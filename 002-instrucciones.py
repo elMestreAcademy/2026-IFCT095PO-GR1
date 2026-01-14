ae688285210fe446eb41fd46be6b5eb24accb95c
elMestre <carmesce@gmail.com>
2026-01-14 20:44:46 +0100

    Sesión 2

---
+++ 002-instrucciones.py
@@ -0,0 +1,26 @@
+4 + 3           # ESTO ES UNA INSTRUCCION
+4 * 3           # ESTO ES UNA 2a INSTRUCCION
+print("patata") # ESTO ES UNA 3a INSTRUCCION
+print(5 * 10)   # ESTO ES UNA 4a INSTRUCCION
+                # La 4a instruccion se compone de
+                # varias expresiones
+                # primero se ejecuta la operacion aritmetica
+                # y luego se llama a la funcion print
+                # print(5 * 10)
+                # print(50)
+                # magia que enseña texto
+
+"""
+en c
+
+
+if (cantidad < limite) { haz_algo(); }
+
+en python
+
+if cantidad < limite:
+    haz_algo()
+
+
+
+"""
---
+++ esquema.md
@@ -0,0 +1,9 @@
+# 4 pilares de la programacion estructurada
+
+- Expresiones (operaciones)
+    - Una expresion es un tipo de operacion que el PROCESADOR debe ejecutar
+
+- Datos y tipos de datos
+    - int (entero)
+- Estructuras de control
+- Funciones

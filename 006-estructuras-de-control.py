e651da9ebc5fb374d9f8374eb51c5278adf181e8
Carlos <carmesce@gmail.com>
2026-01-19 20:51:05 +0100

    E. control

---
+++ 006-estructuras-de-control.py
@@ -0,0 +1,35 @@
+# Ejemplo de if
+"""
+print("EMPEZAMOS")
+
+dinero = 100
+articulo = 36
+
+tenemos_bastante = dinero > articulo
+print(type(tenemos_bastante))
+
+
+# if True:
+if False:
+    print("lo compro")
+else:
+    print("Nos vamos a casa")
+
+print("FIN")
+"""
+
+# Ejemplo de while
+
+## Contar hasta 10
+
+min = 1
+max = 10
+posicion = min
+
+print(posicion)
+while posicion < max:
+    posicion = posicion + 1
+    print(posicion)
+print(">>", posicion, "<<")
+
+## Cuenta atras de un cohete
